7f84f62d8b11ccefafcf3dcf56f06c51eb266db2
Angel Tesillos <atesillos@luc.edu>
2023-05-12 10:56:13 -0500

    began implementation

---
+++ src/beginner/VowelCount.js
@@ -0,0 +1,18 @@
+/*
+Count the Vowels: 
+Write a function that takes a string and 
+returns the number of vowels in the string.
+*/
+
+/*
+Set of vowels will be used to check the string
+which is taken from commandline input!
+As we iterate through the string, we ask if that
+char is inside our set. If yes, add one. Else 
+just iterate to the next one if there is a next.
+*/
+const vowels = new Set(["a","e","i","o","u"]);
+
+array.forEach(element => {
+    
+});
